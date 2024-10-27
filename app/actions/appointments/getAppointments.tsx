f383a8c5d1f3b3b690845c96e587f60649bfbb32
ALAM <alexamara97@gmail.com>
2024-10-27 14:24:57 +0100

    Fix - Users Order

--- app/actions/appointments/getAppointments.tsx
+++ app/actions/appointments/getAppointments.tsx
@@ -10,7 +10,7 @@ async function getAppointments(): Promise<Appointment[]> {
       user: true,
     },
     orderBy: {
-      date: "asc",
+      date: "desc",
     },
   });
 
--- app/actions/user/getUserById.tsx
+++ app/actions/user/getUserById.tsx
@@ -18,7 +18,7 @@ async function getUserById(clientId: string): Promise<SuperUser | null> {
 
   if (user) {
     user.appointments.sort(
-      (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()
+      (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
     );
     return extendUser(user);
   }
