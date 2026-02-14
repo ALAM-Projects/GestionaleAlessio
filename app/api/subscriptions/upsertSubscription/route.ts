0dbb43a8e190d2f64e67a7be6fe1476c5aedfcdf
ALAM <alexamara97@gmail.com>
2026-02-14 11:41:16 +0100

    Build fixes

--- app/api/subscriptions/upsertSubscription/route.ts
+++ app/api/subscriptions/upsertSubscription/route.ts
@@ -29,7 +29,7 @@ export async function POST(request: Request) {
     completed,
     clientId,
     doneAppointments,
-    subscriptionId,
+    subscriptionId != null ? Number(subscriptionId) : undefined,
   );
 
   if (!success) {
